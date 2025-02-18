5faba233dc388406064536f267da43bdef3975ac
malupi20 <maria.malubh3@gmail.com>
2025-02-17 21:40:20 -0500

    subo proyecto final

---
+++ css/script.js
@@ -0,0 +1,65 @@
+document.getElementById('verMasBtn1').addEventListener('click', function() {
+    var info = document.getElementById('info1');
+    if (info.classList.contains('info-hidden')) {
+        info.classList.remove('info-hidden');
+        info.classList.add('info-visible');
+        this.textContent = 'Ocultar información';
+    } else {
+        info.classList.remove('info-visible');
+        info.classList.add('info-hidden');
+        this.textContent = 'Ver más información';
+    }
+});
+
+document.getElementById('verMasBtn2').addEventListener('click', function() {
+    var info = document.getElementById('info2');
+    if (info.classList.contains('info-hidden')) {
+        info.classList.remove('info-hidden');
+        info.classList.add('info-visible');
+        this.textContent = 'Ocultar información';
+    } else {
+        info.classList.remove('info-visible');
+        info.classList.add('info-hidden');
+        this.textContent = 'Ver más información';
+    }
+});
+
+
+document.getElementById('verMasBtn3').addEventListener('click', function() {
+    var info = document.getElementById('info3');
+    if (info.classList.contains('info-hidden')) {
+        info.classList.remove('info-hidden');
+        info.classList.add('info-visible');
+        this.textContent = 'Ocultar información';
+    } else {
+        info.classList.remove('info-visible');
+        info.classList.add('info-hidden');
+        this.textContent = 'Ver más información';
+    }
+});
+
+document.getElementById('verMasBtn4').addEventListener('click', function() {
+    var info = document.getElementById('info4');
+    if (info.classList.contains('info-hidden')) {
+        info.classList.remove('info-hidden');
+        info.classList.add('info-visible');
+        this.textContent = 'Ocultar información';
+    } else {
+        info.classList.remove('info-visible');
+        info.classList.add('info-hidden');
+        this.textContent = 'Ver más información';
+    }
+});
+
+document.getElementById('verMasBtn5').addEventListener('click', function() {
+    var info = document.getElementById('info5');
+    if (info.classList.contains('info-hidden')) {
+        info.classList.remove('info-hidden');
+        info.classList.add('info-visible');
+        this.textContent = 'Ocultar información';
+    } else {
+        info.classList.remove('info-visible');
+        info.classList.add('info-hidden');
+        this.textContent = 'Ver más información';
+    }
+});
